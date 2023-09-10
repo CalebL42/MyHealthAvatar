7174569ebb5eb39db89d059488a1e1412fe3947d
CalebL42 <calebee0462@gmail.com>
2023-09-09 20:27:18 -0700

    I added an input box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MyHealthyAvatar
+          Welcome to MyHealthyAvatar I like hammies
         </p>
+        Input your answer here poober:
+        <input type = "text"/>
       </header>
     </div>
   );
