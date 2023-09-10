47de67275079ee6956ecc4c87e747fe9291eca44
CalebL42 <calebee0462@gmail.com>
2023-09-10 05:03:01 -0700

    added css styling for the game page: background, button and text colors, etc

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -20,24 +20,45 @@ function Game() {
       avatarImage = null; // Set a default image or handle the case where avatarName is not recognized
   }
 
-  const [count, setCount] = useState(0);
-  const incrementCount = () => {
-    setCount(count + 1);
+  const questions = [
+    "Did you get at least 7 hours of restful sleep last night?",
+    "Have you been getting regular exercise or physical activity?",
+    "Do you eat a balanced diet with a variety of foods?",
+    "Are you staying hydrated throughout the day?",
+    "Have you talked to someone about your feelings lately?"
+    // Add more questions here
+  ];
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [wellnessLevel, setWellnessLevel] = useState(0);
+
+  const incrementWellnessLevel = () => {
+    setWellnessLevel(wellnessLevel + 100);
+    setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
+  };
+
+  const decrementWellnessLevel = () => {
+    setWellnessLevel(wellnessLevel - 100);
+    setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
   };
 
   return (
-      <div className = "Game">
+    <div className = "Game">
       <header className = "Game-header">
-        <p>
-          MyHealthyAvatar
-        </p>
-        <img src={avatarImage} alt={avatarName} />
-        <div>
-        <p>Number: {count}</p>
-        <button onClick={incrementCount}>Increment</button>
-      </div>
+      <p>MyHealthyAvatar</p>
       </header>
+      <div className="question-container">
+        <p className="question">{questions[currentQuestionIndex]}</p>
+        <button className = "yes_button" onClick={incrementWellnessLevel}>Yes</button>
+        <button className = "no_button" onClick={decrementWellnessLevel}>No</button>
       </div>
+      <div className="avatar">
+        <img src={avatarImage} alt={avatarName} />
+      </div>
+      <div className="wellness">
+        <p>Wellness level: {wellnessLevel}</p>
+      </div>
+    </div>
   );
 }
 
